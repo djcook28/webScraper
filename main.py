ac24c96361a0c9035547169dcc56ebecf7ba1087
dcook <daniel.j.cook.28@gmail.com>
2025-06-15 14:33:54 -0500

    added data.txt which retains previously scrapped results.  New results are compared against previous results and a new email is only triggered if it's a truly new event

--- main.py
+++ main.py
@@ -13,15 +13,26 @@ def extraction(source):
     value = extractor.extract(source)["tours"]
     return value
 
+def store_extracted(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted+"\n")
+
+def load_previous_extracted():
+    with open("data.txt", "r") as file:
+        content = file.read()
+        return content
+
 def send_email(extracted):
     print("email sent")
     print(extracted)
 
 if __name__== "__main__":
+    previous_data = load_previous_extracted()
     while True:
         scraped_source = scrape(URL)
         extracted = extraction(scraped_source)
-        if(extracted != "No upcoming tours"):
+        if(extracted != "No upcoming tours" and extracted not in previous_data):
+            store_extracted(extracted)
             send_email(extracted)
             break
         else:
