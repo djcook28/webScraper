ccf6191fbb3d9aaa29d4531400069c5fe3dec4fb
dcook <daniel.j.cook.28@gmail.com>
2025-06-21 15:51:53 -0500

    changed data pull to only check for the extracted value rather than loading the whole sql db

--- data_manager.py
+++ data_manager.py
@@ -3,10 +3,15 @@ import sqlite3
 connection = sqlite3.connect("events.db")
 cursor = connection.cursor()
 
-def load_previous_extracted():
+
+def format_date(extracted):
+    extracted_tuple = tuple(extracted.split(","))
+    return [item.strip() for item in extracted_tuple]
+
+def load_previous_extracted(data_tuple):
     #with open("data.txt", "r") as file:
     #   return file.read()
-    cursor.execute("SELECT * FROM events")
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", data_tuple)
     return (cursor.fetchall())
 
 def store_extracted(data_tuple):
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import emailer
 URL = "https://programmer100.pythonanywhere.com/tours"
 
 if __name__== "__main__":
-    previous_data = data_manager.load_previous_extracted()
     while True:
         extracted = scraper.extraction(URL)
-        extracted_tuple = tuple(extracted.split(", "))
-        if(extracted != "No upcoming tours" and extracted_tuple not in previous_data):
-            emailer.send_email(extracted)
-            data_manager.store_extracted(extracted_tuple)
-            break
-        else:
-            print("no email")
+        if(extracted != "No upcoming tours"):
+            extracted_tuple = data_manager.format_date(extracted)
+            previous_data = data_manager.load_previous_extracted(extracted_tuple)
+            if(not previous_data):
+                emailer.send_email(extracted)
+                data_manager.store_extracted(extracted_tuple)
+                break
+            else:
+                print("no email")
